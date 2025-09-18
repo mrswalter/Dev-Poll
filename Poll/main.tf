d505df30b88742abaa7833d9c9c11a9ab8ace069
mrswalter404 <mrswalter404@yahoo.com>
2025-09-18 03:31:05 -0500

    modified albsg

--- Poll/main.tf
+++ Poll/main.tf
@@ -20,7 +20,7 @@ module "alb" {
   project_name   = var.project_name
   vpc_id         = module.vpc.vpc_id
   public_subnets = module.vpc.public_subnets
-  alb_sg_id      = module.security_group.alb_sg_id
+  alb_sg_id      = module.security_groups.alb_sg_id
 }
 
 # -----------------------------
