b7a433ce3091e01a5d2a9a49ca51a8512dc877e5
mrswalter404 <mrswalter404@yahoo.com>
2025-09-18 01:50:52 -0500

    modified main

--- Poll/main.tf
+++ Poll/main.tf
@@ -16,10 +16,11 @@ module "vpc" {
 # Load Balancer (ALB)
 # -----------------------------
 module "load_balancer" {
-  source         = "./modules/load_balancer"
+  source         = "./modules/alb"
   project_name   = var.project_name
   vpc_id         = module.vpc.vpc_id
   public_subnets = module.vpc.public_subnets
+  alb_sg_id      = module.vpc.alb_sg_id
 }
 
 # -----------------------------
